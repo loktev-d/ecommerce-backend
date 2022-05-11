d47af50728afc24ed732756b17bfc7288c5284f0
Daniil Loktev <lokt.daniil@gmail.com>
2022-05-11 23:49:27 +0300

    Add outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -250,3 +250,19 @@ resource "aws_instance" "k8s_worker_2" {
     Name = "k8s_worker_2"
   }
 }
+
+output "k8s_master_public_ip" {
+  value = aws_instance.k8s_master.public_ip
+}
+
+output "k8s_master_private_ip" {
+  value = aws_instance.k8s_master.private_ip
+}
+
+output "k8s_worker_1_private_ip" {
+  value = aws_instance.k8s_worker_1.private_ip
+}
+
+output "k8s_worker_2_private_ip" {
+  value = aws_instance.k8s_worker_2.private_ip
+}
